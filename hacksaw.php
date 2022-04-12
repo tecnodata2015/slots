26e551cc2b19607c814f72fa1213f4b334d2b750
Jonny Rodriguez <jojoroto@gmail.com>
2022-04-12 05:06:31 +0000

    hacksaw

--- hacksaw.php
+++ hacksaw.php
@@ -8,16 +8,18 @@ $csv = trim(str_replace("\r", '', $result), "\n");
 $rows = explode("\n", $csv);
 foreach ($rows as $row) {
     $r = explode('/', $row);
-    $n = preg_replace("/[^a-z]+/", "", strtolower($r[3]));
+    $n = preg_replace("/[^a-z0-9]+/", "", strtolower($r[3]));
     $files[$n] = $row;
 }
+print_r($files);
+#die();
 
 $result = file_get_contents('hacksaw.csv');
 $csv = trim(str_replace("\r", '', $result), "\n");
 $rows = explode("\n", $csv);
 foreach ($rows as $row) {
     $r = explode(',', $row);
-    $n = preg_replace("/[^a-z]+/", "", strtolower($r[1]));
+    $n = preg_replace("/[^a-z0-9]+/", "", strtolower($r[1]));
     if(isset($files[$n])) {
         $fname = "hacksaw/$r[0].png";
         $c="mv \"$files[$n]\" $fname";
